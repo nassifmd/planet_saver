6d2ba6e967538373be7ce99a4f234777a152b39c
mac <mac@mbp-von-mac.speedport.ip>
2024-02-12 16:48:59 +0100

    updated

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,7 +6,7 @@
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <title>PlanetSavers: Rise of the Green Guardians</title>
 </head>
-<body class="font-sans bg-gray-100 relative overflow-hidden">
+<body class="font-sans bg-gray-100 relative overflow-hidden bg-cover bg-no-repeat bg-center" style="background-image: url('/img/2.png');">
 
     <style>
         /* Keyframes for animation */
@@ -42,7 +41,7 @@
 
     </style>
 
-    <div class="h-screen flex items-center justify-center bg-blue-500 text-white relative">
+    <div class="h-screen flex items-center justify-center text-white relative">
         <div class="flex-auto w-64 text-center lg:text-left lg:w-1/2 p-8 z-10">
             <h1 class="text-4xl font-bold mb-4">PlanetSavers: Rise of the Green Guardians</h1>
             <p class="text-lg mb-8">Explore the adventure</p>
@@ -52,25 +51,23 @@
             </div>
         </div>
         
-    <div class="text-justify absolute top-1/2 right-20 transform -translate-y-1/2 flex flex-col items-center">
-        <img src="/img/team/nas.jpg" alt="Floating Image" class="h-auto opacity-50">
-        <div class="text-white fixed top-1/2 right-0 transform -translate-y-1/2 z-20 mx-7 scroll_effect text-animations">
-            <p class="text-lg font-bold mb-2 text-center">About</p>
-            <p class="text-sm">Welcome to PlanetSavers, where players become eco-heroes in a battle for a greener future. Armed with sustainable superpowers, you'll tackle real-world environmental challenges, earn eco-points, and rise through the ranks. Join a global community dedicated to making a positive impact and redefine heroism in the quest for a sustainable tomorrow. The journey starts now at PlanetSavers.</p>
-            
-            <p class="text-lg font-bold mt-4 mb-2 text-center">Game Instructions</p>
-            <ol class="text-sm">
-                <li>Choose your eco-hero and sustainable superpower from the options provided.</li>
-                <li>Navigate through different levels, each representing a unique environmental challenge.</li>
-                <li>Use your sustainable superpower strategically to overcome obstacles and enemies.</li>
-                <li>Earn eco-points by completing challenges, solving puzzles, and making eco-friendly choices.</li>
-                <li>Collaborate with other players in the global community to tackle larger environmental issues.</li>
-                <li>Rise through the ranks and unlock new abilities as you make a positive impact on the virtual world.</li>
-                <li>Remember, every action counts - play responsibly and contribute to a sustainable tomorrow!</li>
-            </ol>
+        <div class="text-justify absolute right-10 transform -translate-y-1/2 flex flex-col items-center">
+            <div class="text-white fixed top-1/2 right-20 w-96 transform -translate-y-1/2 z-20 mx-7 scroll_effect text-animations">
+                <p class="text-lg font-bold mb-2 text-center">About</p>
+                <p class="text-sm">Welcome to PlanetSavers, where players become eco-heroes in a battle for a greener future. Armed with sustainable superpowers, you'll tackle real-world environmental challenges, earn eco-points, and rise through the ranks. Join a global community dedicated to making a positive impact and redefine heroism in the quest for a sustainable tomorrow. The journey starts now at PlanetSavers.</p>
+                
+                <p class="text-lg font-bold mt-4 mb-2 text-center">Game Instructions</p>
+                <ol class="text-sm justify-center">
+                        <li>Select the appropriate option for each question.</li>
+                        <li>You will receive 1 point for each correct answer.</li>
+                        <li>You will lose 1 point for each wrong answer.</li>
+                        <li>Click the "Stop Game" button to end the game and submit your score.</li>
+                        <li>When you complete the game, you have the chance of playing again.</li>
+                        <li>If you are the top player of the month, you can redeem a tree of your choice.</li>
+                        <li>Tree will be sent to you at your selected location for planting</li>
+                </ol>
+            </div>
         </div>
-    </div>
-        
     </div>
 
 </body>
